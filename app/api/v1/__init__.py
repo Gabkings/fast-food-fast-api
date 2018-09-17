808c28384aa34a04d508269b965a7f8c4711da43
unknown <gabworks51@gmail.com>
2018-09-17 16:45:40 +0300

    feature (description) This displays all the orders available in the orders list
    [delivers #160537236]

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Api
+from config import app_config
+from .orders import OrderDetals,DisplayAllOrders
+#from .users import DisplayAllUsers,UserCreation,UserDetails
+def create_app(config_stage):
+    Gabriel = Flask(__name__)
+    app.config.from_object(app_config[config_stage])
+    api = Api(app)
+    '''Versioning of the endpoints url'''
+    api.add_resource(OrderDetals, '/api/v1/orders/<int:id>')
+    api.add_resource(DisplayAllOrders, '/api/v1/orders')
+    return Gabriel
